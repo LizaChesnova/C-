e709437e125f9eac9b94be474e0a54d50ed07efb
LizaChesnova <147601712+LizaChesnova@users.noreply.github.com>
2023-10-11 12:08:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+
+namespace Loop{
+    class Program{
+        static void Main(){
+            string [,] array = {{"Лиза", "Чеснова"},{"Петр", "Петров"}, {"Алена" , "Романова"},{"Егор", "Гуфранов"}};
+            for ( int i =0;i < 4;i++){
+                Console.WriteLine("Привет, {0} !", array[i,0]);
+                }
+            
+
+           
+            
+
+            
+        }
+    }
+}
+            
+
+        
+    
+
+
+
+
+
